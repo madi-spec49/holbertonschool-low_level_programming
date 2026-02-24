a59b0fddc210d4b48905985587cd16cc9f0202fd
madi-spec49 <madi.anli.madi@gmail.com>
2026-02-24 15:50:48 +0000

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts ("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: displays a sentence using the puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
