dc6ede2827c8a139fcf98ca057258f481d99ef4f
madi-spec49 <madi.anli.madi@gmail.com>
2026-02-26 16:35:31 +0000

    fix code: inversion comparaison

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,21 +5,30 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Description: Assigns a random number to n and prints
+ * whether it is positive, zero, or negative.
  *
- * Description: Rand number 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n );
-		else if (0 == 0)
-			     printf("%d is zero\n", n );
-	if (n < 0)
-		printf("%dis negative\n", n );
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative\n", n);
+    }
+    else
+    {
+        printf("%d is zero\n", n);
+    }
+
+    return (0);
 }
