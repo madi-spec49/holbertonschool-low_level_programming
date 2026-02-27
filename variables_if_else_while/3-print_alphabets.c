9f14d41004d3767e2b97928f81d855d87c372ce6
madi-spec49 <madi.anli.madi@gmail.com>
2026-02-27 03:45:44 +0000

    alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -14,13 +14,13 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 
-	putchar (letter);
+	putchar(letter);
 	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 
-		putchar (letter);
+		putchar(letter);
 
-	putchar ('\n');
+	putchar('\n');
 		
 	return (0);
 }
