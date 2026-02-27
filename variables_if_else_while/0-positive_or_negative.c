f264c24cd10457576b4504dbc1ec813de355931c
username <user-email>
2026-02-27 09:20:01 +0000

    fix code: inveresion comparaison

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Description: Assigns a random number to n and prints
+ *
  * whether it is positive, zero, or negative.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else
-    {
-        printf("%d is zero\n", n);
-    }
+	if (n > 0)
 
-    return (0);
+		printf("%d is positive\n", n);
+
+	else if (n < 0)
+
+		printf("%d is negative\n", n);
+
+	else
+
+		printf("%d is zero\n", n);
+
+	return (0);
 }
