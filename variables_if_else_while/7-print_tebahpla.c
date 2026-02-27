5d67867a1d915f150fffc05804b175e3ed535649
username <user-email>
2026-02-27 13:27:57 +0000

    in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Degression
+ *
+ * Return: Always: 0 (Success)
+ *
+ */
+int main(void)
+
+{
+	char n;
+
+	for (n ='z'; n >= 'a'; n--)
+
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
