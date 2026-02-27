1a639bf679cdcd829557f06c5379d16049c38ffb
username <user-email>
2026-02-27 09:01:00 +0000

    number0_10

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* 
-* Description: prints all single digit numbers 
+*
+* Description: prints all single digit numbers
 *
 * Return: Always 0 (Success)
 *
 */
-int main(void) 
-
+int main(void)
 {
 	char n;
-	for (n='0'; n<='9'; n++)
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
 	putchar('\n');
-	return (0);       
-}	
-
+	return (0);
+}
