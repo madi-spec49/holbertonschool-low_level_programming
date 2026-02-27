0ad90b794ed52664dc02fbfa7f26a0be20797912
username <user-email>
2026-02-27 10:40:56 +0000

    nmbr0_10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single numbers
+ *
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
