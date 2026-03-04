5fbd1994b9791eea0a84b8882a0bf6e49a056aeb
username <user-email>
2026-03-04 12:14:00 +0000

    reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+*
+*  Description: a function that takes a pointer to an int
+*
+* Return: Always 0 (success)
+*
+*/
+void reset_to_98(int *n)
+
+{
+
+	*n = 98;
+}
