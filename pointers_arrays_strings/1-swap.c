79cc503a92ab7c579e9f09afc4e17e37731f20b3
username <user-email>
2026-03-05 15:28:23 +0000

    swaps the values

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
+* swap_int - Swaps the values of two integers
 *
-*  Description: a function permute a values
+* Description: a function permute a values
 *
-*
+* @a: pointer to the first integer
+* @b: pointer to the second integer
 *
 */
 void swap_int(int *a, int *b)
-
 {
 	int c;
 
