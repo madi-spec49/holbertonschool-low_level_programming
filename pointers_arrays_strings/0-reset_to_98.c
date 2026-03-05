fb9d42bcc4fa0165a40537d7d4e45fa29831b55b
username <user-email>
2026-03-05 14:54:37 +0000

    fix betty style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-* main - entry point
+* reset_to_98 - entry point
 *
-*  Description: a function that takes a pointer to an int
+* Descrition: a function that takes a pointer to an int
 *
+* @n:pointer to reset
 * Return: Always 0 (success)
 *
 */
+
 void reset_to_98(int *n)
 
 {
