5c2917e74c6110df9b4241129463549478b741f6
username <user-email>
2026-03-05 00:53:29 +0000

    Isaac Asimov

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - retourne la longueur d'une chaine
+*
+* Description:la chaine de caractere
+*
+* longeur de la chaine
+*
+*/
+
+void _puts(char *s)
+
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+}
