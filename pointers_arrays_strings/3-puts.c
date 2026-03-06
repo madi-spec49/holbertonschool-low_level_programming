2737beaf92926bf7b0e77728a453a3ac7664c331
username <user-email>
2026-03-06 10:16:16 +0000

    fix betty style

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-* _puts- Returns the length of a string
-* @s: pointer to the string
-*
+* 
+* _puts - prints a string, followed by a new line
 * Description: function that prints a string, followed by a new line
+* @s: pointer to the string
 *
 * Return: The length of the string.
 *
 */
-
-void _puts(char *s)
-
+void _puts(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		putchar(s[i]);
-		i++;
-	}
-	putchar('\n');
+    while (str[i] != '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
+    putchar('\n');
 }
