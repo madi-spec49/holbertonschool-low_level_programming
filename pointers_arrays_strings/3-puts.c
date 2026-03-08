49e4aba9bd4e8090c3dac7f7dd788c0669f86fa4
username <user-email>
2026-03-08 13:44:52 +0000

    fix betty style

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,17 +3,19 @@
 /**
 *  _puts - Prints a string followed by a new line
 * @str: The string to print
-* 
+*
 *
 */
+
 void _puts(char *str)
 {
-    int i = 0;
+	int i;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-        _putchar('\n');
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
