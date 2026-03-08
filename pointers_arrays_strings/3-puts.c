55daafd65a38906681813f44fb98d2029638b89a
username <user-email>
2026-03-08 14:39:55 +0000

    fix betty style

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,7 +4,9 @@
 *  _puts - Prints a string followed by a new line
 * @str: The string to print
 *
+* Description: Write a function that prints a string, followed by a new line
 *
+* Return: The length of the string.
 */
 
 void _puts(char *str)
