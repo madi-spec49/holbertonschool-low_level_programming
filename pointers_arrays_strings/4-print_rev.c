334b4fd4bef3b9d3dc758b0b52bfe9f5fe04b1ea
username <user-email>
2026-03-08 14:48:25 +0000

    sprint reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _puts - Prints a string
+*
+* @s: The string to print in reverse
+*
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
