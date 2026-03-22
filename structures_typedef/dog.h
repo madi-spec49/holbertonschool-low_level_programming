26f363963901dc855bdd949d94123484a8b5e687
madi-spec49 <madi.anli.madi@gmail.com>
2026-03-22 13:57:07 +0000

    taskone

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 typedef struct dog
+
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 } dog_t;
+#endif
