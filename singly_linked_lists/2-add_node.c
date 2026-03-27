005bfaf2c4dd0a2c3a7107442a569b94f818f422
madi-spec49 <madi.anli.madi@gmail.com>
2026-03-27 13:51:57 +0000

    task2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+	int len = 0;
+
+	while (str[len])
+		len++;
+	newnode = malloc(sizeof(list_t));
+	if (!newnode)
+		return (NULL);
+	newnode->str = strdup(str);
+	if (!newnode->str)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	newnode->len = len;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
