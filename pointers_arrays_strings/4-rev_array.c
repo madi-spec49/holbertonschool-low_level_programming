3dca83d7c2c356ae0caebf859b17bef75dfd1116
madi-spec49 <madi.anli.madi@gmail.com>
2026-04-22 22:27:40 +0200

    task4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: The array to reverse
+ * @n: Number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	j = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+	}
+}
