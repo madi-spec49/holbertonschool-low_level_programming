82e877108f1a051bd05d990c2a646ab20bf62570
madi-spec49 <madi.anli.madi@gmail.com>
2026-04-22 17:32:15 +0200

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -23,11 +23,11 @@ int main(void)
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
