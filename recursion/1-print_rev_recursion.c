8af61390ca9f71082b0bfc4e84cf9c0c1273a659
madi-spec49 <madi.anli.madi@gmail.com>
2026-04-24 11:20:39 +0200

    fix betty

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - a function that prints a string, followed by a new line.
+ * _print_rev_recursion - a function that prints a string in reverse
  * @s: An input string to printing
  * Return: Nothing
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
