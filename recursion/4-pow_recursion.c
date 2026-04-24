bcd1ad813f37ebc07e314cf6432195de10edbabc
madi-spec49 <madi.anli.madi@gmail.com>
2026-04-24 11:24:34 +0200

    fix betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,18 +1,26 @@
 #include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to be raised
+ * @y: power to raise x to
+ *
+ * Return: value of x raised to the power of y
+ */
 
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
+	if (y < 0)
 
-		return(-1);
+		return (-1);
 
-	else if(y == 0)
+	else if (y == 0)
 
-		return(1);
+		return (1);
 
 	else
 
-		return(x * _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 
 }
+
 
